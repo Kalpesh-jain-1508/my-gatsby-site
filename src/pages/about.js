f3257f35573e9f081dcd07fb745d826140e9eed5
Kalpesh Jain <kalpesh@hastishah.com>
2022-02-15 12:14:36 +0530

    pages and components
    add component and about page link

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import { Link } from 'gatsby'
+import React, { Component } from 'react'
+
+const AboutPage = () => {
+
+    return (
+        <main>
+            <Link to='/'>HomePage</Link>
+
+            <h3>About Us Page</h3>
+        </main>
+    )
+        
+}
+
+export default AboutPage
